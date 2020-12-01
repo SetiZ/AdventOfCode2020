bbad3fa4f586d9f5677eb2bd58699cb8e3839b29
SetiZ <SetiZ@users.noreply.github.com>
2020-12-01 12:03:44 +0100

    day 1-2

--- day1/day1.js
+++ day1/day1.js
@@ -5,8 +5,10 @@ input = input.map((m) => parseInt(m, 10));
 // console.log(input)
 for (let i = 0; i <input.length; i++) {
   for (let j = i + 1; j <input.length - 1; j++) {
-    if (input[i] + input[j] === twentytwenty) {
-      console.log(input[i] * input[j])
+    for (let k = j + 1; k <input.length - 2; k++) {
+      if (input[i] + input[j] + input[k]=== twentytwenty) {
+        console.log(input[i] * input[j] * input[k])
+      }
     }
   }
 }
