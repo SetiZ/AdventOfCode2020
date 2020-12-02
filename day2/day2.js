eef162003882cf6d4e5e70d4036368a25ce619c6
SetiZ <SetiZ@users.noreply.github.com>
2020-12-02 14:39:11 +0100

    day 2-2

--- day2/day2.js
+++ day2/day2.js
@@ -6,9 +6,10 @@ input.map((m) => {
   const line = m.split(' ');
   const limits = line[0].split('-').map(Number);
   const letter = line[1].substring(0, 1);
-  const length = (line[2].match(new RegExp(letter, "g")) || []).length;
-  if (length !== 0 && length >= limits[0] && length <= limits[1]) {
-    numbers++
+  const char1 = line[2].charAt(limits[0]-1)
+  const char2 = line[2].charAt(limits[1]-1)
+  if (letter === char1 ^ letter === char2) {
+    numbers++;
   }
 });
 
