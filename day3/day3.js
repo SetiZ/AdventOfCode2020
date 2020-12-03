88fe8144426c8f90307b1e5f49dcda34efeb7456
SetiZ <SetiZ@users.noreply.github.com>
2020-12-03 14:22:04 +0100

    day 3-2

--- day3/day3.js
+++ day3/day3.js
@@ -1,21 +1,36 @@
 const fs = require('fs');
 const twentytwenty = 2020;
 let input = fs.readFileSync('input').toString().split("\n");
-let i = 1;
-let j = 0;
-let trees = 0;
-while (i < input.length) {
-  let line = input[i].split('');
-  i++;
-  j += 3;
-  // console.log(line.length, j)
-  if (j >= line.length) {
-    j = j - line.length;
-  }
-  // console.log('line ' + i, line[j])
-  if (line[j] === '#') {
-    trees++
-  }
-}
+let multiTrees = []
+const slopes = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2]
+]
 
-console.log(trees);
+slopes.forEach((slope) => {
+  let i = 0;
+  let j = 0;
+  let trees = 0;
+  while (i < input.length - slope[1]) {
+    // console.log(i)
+    i += slope[1];
+    j += slope[0];
+    let line = input[i].split('');
+    if (j >= line.length) {
+      j = j - line.length;
+    }
+    // console.log(line.length, j)
+    // console.log('line ' + i, line[j])
+    if (line[j] === '#') {
+      trees++
+    }
+  }
+  multiTrees.push(trees)
+  // console.log(trees);
+})
+
+// console.log(multiTrees);
+console.log(multiTrees.reduce((a, b)=> a*b, 1))
