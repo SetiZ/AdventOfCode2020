7e424aa8968946f7842cad9588173f8e1789ce2f
SetiZ <SetiZ@users.noreply.github.com>
2020-12-06 20:56:42 +0100

    day 6-2

--- day6/day6.js
+++ day6/day6.js
@@ -2,11 +2,28 @@ const fs = require('fs');
 let input = fs.readFileSync('input').toString().split("\n\n");
 let lengthAll = 0;
 input.forEach((line) => {
-  let hop = line.split(/\s+/).join('').split('');
+  let hop = line.split(/\s+/);
   // console.log(hop)
-  const unique = [...new Set(hop)];
+  // const unique = [...new Set(hop)];
   // console.log(unique)
-  lengthAll += unique.length
+  // lengthAll += unique.length
+  if (hop.length === 1) {
+    lengthAll += hop[0].length
+  } else {
+    // console.log(hop)
+    const res = hop.reduce((a, b) => {
+      let resultstring = [];
+      for (let i of a){
+          for(let n of b){
+              if( n === i){
+                  resultstring.push(n);
+              }
+          }
+      };
+      return resultstring;
+    })
+    lengthAll += res.length
+  }
 });
 
 console.log(lengthAll)
