afa36c728370c3bf865b73a8793083935a33badd
SetiZ <SetiZ@users.noreply.github.com>
2020-12-11 12:08:55 +0100

    day 8-2

--- day8/day8.js
+++ day8/day8.js
@@ -9,28 +9,65 @@ const instructions = input.map((line) => {
 let accumumlator = 0;
 let step = 0;
 
-while (step < instructions.length && step >=0) {
-  if (!instructions[step][2]) {
-    switch (instructions[step][0]) {
-      case 'nop':
-        instructions[step][2] = true
-        step++;
-        break;
-      case 'acc':
-        instructions[step][2] = true
-        accumumlator += +instructions[step][1]
-        step++;
-        break;
-      case 'jmp':
-        instructions[step][2] = true
-        step += +instructions[step][1]
-        break;
+const stepper = (instruc) => {
+  step = 0;
+  accumumlator = 0;
+  // console.log(instruc)
+  while (step < instruc.length && step >=0) {
+    if (!instruc[step][2]) {
+      // console.log(instruc[step])
+      switch (instruc[step][0]) {
+        case 'nop':
+          instruc[step][2] = true
+          step++;
+          break;
+        case 'acc':
+          instruc[step][2] = true
+          accumumlator += +instruc[step][1]
+          step++;
+          break;
+        case 'jmp':
+          instruc[step][2] = true
+          step += +instruc[step][1]
+          break;
+      }
+    } else {
+      break;
     }
-  } else {
-    break;
+    // console.log(accumumlator, step)
   }
-  // console.log(accumumlator, step)
-  // console.log(instructions)
+}
+
+let temp = JSON.parse(JSON.stringify(instructions));
+let i = 0;
+while (i < instructions.length) {
+  if (instructions[i][0] === 'nop') {
+    temp = JSON.parse(JSON.stringify(instructions));
+    temp[i][0] = 'jmp';
+    // console.log(temp)
+    stepper(temp);
+    // console.log(temp)
+    if (temp[temp.length-1][2]) {
+      console.log(accumumlator, step)
+    }
+  }
+  i++
+}
+
+temp = JSON.parse(JSON.stringify(instructions));
+i = 0
+while (i < instructions.length) {
+  if (instructions[i][0] === 'jmp') {
+    temp = JSON.parse(JSON.stringify(instructions));
+    temp[i][0] = 'nop';
+    // console.log(temp)
+    stepper(temp);
+    // console.log(temp)
+    if (temp[temp.length-1][2]) {
+      console.log(accumumlator, step)
+    }
+  }
+  i++
 }
 
 console.log(accumumlator)
